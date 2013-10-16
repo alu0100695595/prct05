372acc5edff10c1203c8620e2e1f540e06e1151f
Daniel Gutiérrez <alu0100695595@ull.edu.es>
2013-10-16 18:07:35 +0100

    Todas las operaciones implementadas

--- racional.rb
+++ racional.rb
@@ -11,28 +11,42 @@ class Fraccion
 	end
 	
 	def to_s 
-		puts "Fracción: "
-		puts "#{@n}/#{@d}"
-		puts "Fracción minimizada: "
-		puts "#{@n/gcd(@n,@d)}/#{@d/gcd(@n,@d)}"
+		"#{@n/gcd(@n,@d)}/#{@d/gcd(@n,@d)}"
 	end
+	
+	def +(other)
+		if(@d == other.d)
+		  Fraccion.new(@n + other.n,@d)
+		else
+		  Fraccion.new((@n*other.d)+(@d*other.n),@d*other.d)
+		end
+	end
+	
+	def -(other)
+		if (@d == other.d)
+		  Fraccion.new(@n - other.n,@d)
+		else
+		  Fraccion.new((@n*other.d)-(@d*other.n),@d*other.d)
+		end
+	end
+	
 	def *(other)
 		Fraccion.new(@n * other.n,@d * other.d)
 	end
+	
 	def /(other)
 		Fraccion.new(@n * other.d,@d * other.n)
 	end
 	  
 end
 
-f1 = Fraccion.new(2, 5)
-f2 = Fraccion.new(3, 4)
-f3 = f1*f2
-f4 = f1/f2
+f1 = Fraccion.new(3, 2)
+f2 = Fraccion.new(5, 4)
 
-puts f1.to_s
-puts f2.to_s
-puts "Multiplicacion: "
-puts f3.to_s
-puts "Division: "
-puts f4.to_s
+puts "Fracción 1: #{f1.to_s}"
+puts "Fracción 2: #{f2.to_s}"
+puts "Suma: #{f1} + #{f2} = #{f1+f2}"
+puts "Resta: #{f1} - #{f2} = #{f1-f2}"
+puts "Multiplicación: #{f1} * #{f2} = #{f1*f2}"
+puts "División: #{f1} / #{f2} = #{f1/f2}"
+
