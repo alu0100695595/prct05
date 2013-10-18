171494b693170f5290d2036585285ca8e76d91f9
Nati <alu0100695595@ull.edu.es>
2013-10-18 19:18:16 +0100

    Añadidos tests para cada operacion

--- tc_racional.rb
+++ tc_racional.rb
@@ -4,19 +4,49 @@ require "./racional.rb"
 require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
+  
     def test_simple
       assert_equal("7/2",Fraccion.new(7,2).to_s)
       assert_equal("1/3",Fraccion.new(2,6).to_s)
-      assert_equal("23/6",(Fraccion.new(7,2) + Fraccion.new(2,6)).to_s)
-      assert_equal("19/6",(Fraccion.new(7,2)-Fraccion.new(2,6)).to_s)
-      assert_equal("7/6",(Fraccion.new(7,2)*Fraccion.new(2,6)).to_s)
-      assert_equal("21/2",(Fraccion.new(7,2)/Fraccion.new(2,6)).to_s)
     end
+    
+    def test_sum
+      assert_equal("23/6",(Fraccion.new(7,2) + Fraccion.new(2,6)).to_s)
+      assert_equal("4/1",(Fraccion.new(7,2) + Fraccion.new(3,6)).to_s)
+    end
+    
+    def test_res
+      assert_equal("19/6",(Fraccion.new(7,2)-Fraccion.new(2,6)).to_s)
+      assert_equal("10/3",(Fraccion.new(7,2)-Fraccion.new(1,6)).to_s)
+    end
+    
+    def test_mul
+      assert_equal("7/6",(Fraccion.new(7,2)*Fraccion.new(2,6)).to_s)
+      assert_equal("21/8",(Fraccion.new(7,2)*Fraccion.new(3,4)).to_s)
+    end
+    
+    def test_div
+      assert_equal("21/2",(Fraccion.new(7,2)/Fraccion.new(2,6)).to_s)
+      assert_equal("6/5",(Fraccion.new(2,5)/Fraccion.new(2,6)).to_s)
+    end
+    
     def test_failure
       assert_equal("2/6",Fraccion.new(2,6).to_s)
-      #assert_equal("5/6",(Fraccion.new(7,2)*Fraccion.new(2,6)).to_s)
-      #assert_equal("7/2",(Fraccion.new(7,2)+Fraccion.new(2,6)).to_s)
-      #assert_equal("1/3",(Fraccion.new(7,2)-Fraccion.new(2,6)).to_s)
-      #assert_equal("3/3",(Fraccion.new(7,2)/Fraccion.new(2,6)).to_s)
+    end
+    
+    def test_failure_sum
+      assert_equal("7/2",(Fraccion.new(7,2)+Fraccion.new(2,6)).to_s)
+    end
+    
+    def test_failure_res
+      assert_equal("1/3",(Fraccion.new(7,2)-Fraccion.new(2,6)).to_s)
+    end
+    
+    def test_failure_mul
+      assert_equal("5/6",(Fraccion.new(7,2)*Fraccion.new(2,6)).to_s)
+    end
+    
+    def test_failure_div
+      assert_equal("3/3",(Fraccion.new(7,2)/Fraccion.new(2,6)).to_s)
     end
 end
